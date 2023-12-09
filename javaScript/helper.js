efd42e3417a289e586b1fefe130ee01feaec0cb4
Neda Vaheb <n.vaheb.nv@gmail.com>
2023-12-09 07:26:05 -0800

    function start and show question is add to question.js

--- javaScript/helper.js
+++ javaScript/helper.js
@@ -1,18 +1,16 @@
-const formatData =(questionData)=>{
+const formatData = (questionData)=>{
     
     const result = questionData.map((item)=>{
-        const questionObject = {question : item.question};
-        const AnswerList = [... item.incorrect_answers];
+        const questionObject = { question : item.question }
+        const answers = [... item.incorrect_answers];
         const correctAnswerIndex = Math.floor(Math.random()*4);
-        AnswerList.splice(correctAnswerIndex ,0 , item.correct_answer );
-        questionObject.Answers = AnswerList;
-        questionObject.correctAnswer =  correctAnswerIndex;
+        answers.splice(correctAnswerIndex , 0 , item.correct_answer);
+        questionObject.answers = answers;
+        questionObject.correctAnswerIndex = correctAnswerIndex;
+        
         return questionObject;
-
-
     });
-
     return result;
-};
 
+}
 export default formatData;
--- javaScript/question.js
+++ javaScript/question.js
@@ -1,10 +1,39 @@
 import formatData from "./helper.js";
 
-const URL = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+const loader =document.getElementById("loader");
+const container =document.getElementById("container");
+const questionNumber =document.querySelector("question-number");
+const questionText =document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
 
-const showQuestion = async()=>{
+const URL = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+let formattedData =null;
+let questionIndex = 0;
+let correctAnswer = null;
+
+const fectchData = async ()=>{
 const response = await fetch(URL);
 const json = await response.json();
 console.log(json);
+formattedData = formatData(json.results);
+start();
 }
+const start = ()=>{
+loader.style.display = "none";
+container.style.display = "block";
 showQuestion();
+}
+const showQuestion = ()=>{
+// questionNumber.innerText =  questionIndex + 1;
+const {question, answers , correctAnswerIndex} = formattedData[questionIndex];
+correctAnswer = correctAnswerIndex;
+console.log(correctAnswer);
+questionText.innerText = question;
+answerList.forEach((button,index)=>{
+   button.innerText = answers[index];
+}
+)
+
+
+}
+fectchData();
