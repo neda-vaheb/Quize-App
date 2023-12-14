fad37493e678925e0d7fa980fee54ef682a72338
Neda Vaheb <n.vaheb.nv@gmail.com>
2023-12-13 23:46:48 -0800

    [200~

--- javaScript/end.js
+++ javaScript/end.js
@@ -1,17 +1,23 @@
 const score = JSON.parse(localStorage.getItem("score"));
-
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const gameScore = document.getElementById("game-score");
 const input = document.getElementById("input-user");
 const saveButton = document.querySelector("button");
-let user = [];
+
 gameScore.innerText = score;
  const saveHandler =()=>{
     if(!input.value || !score){
     alert("Invalid Score or Username ")
 
     }else{
-        const finalScore = {name : input.value, score}
-      
+        const finalScore = {name : input.value, score};
+        highScores.push(finalScore);
+        highScores.splice(10);
+        highScores.sort((a,b)=>b.score - a.score);
+        localStorage.setItem("highScores" , JSON.stringify(highScores));
+        localStorage.removeItem("score");
+        window.location.assign("/");
+
     }
 
  }
